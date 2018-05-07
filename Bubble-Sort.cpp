0e59577ec9a7cab057cdf162da2501a2c4461d11
Will <will.green98@hotmail.com>
2018-05-07 15:44:33 +0100

    Just Algorithm => Deleted JSON

--- Bubble-Sort.cpp
+++ Bubble-Sort.cpp
@@ -1,34 +1,17 @@
 #include <iostream>
-#include <fstream>
-
-int read_JSON() {
-    std::ifstream data_sets("/Data/data_sets.json", std::ifstream::binary);
-    data_sets >> data;
-    
-    return data;
-}
 
 void bubble_Sort(int d[]) {
-	d[] = data;
-	
-	for(int a=0; a < data.lenght + 1; a++) {
-		for(int b=1; b < data.lenght + 1; b++) {
-			if(data[a] < data[b]) { 
-				return 0; 
-			} else {
+	for(int a=0; a < data.length() + 1; a++) {
+		for(int b=1; b < data.length() + 1; b++) {
+			if(data[a] > data[b]) { 
 				int checker = data[b];
 				data[b] = data[b+1];
 				data[b+1] = checker;
 			}
 		}
 	}
-	
-	std::cout << bubble_Sort(d[])
 }
 
 int main(int argc, char* argv[]) {
-    std::cout << bubble_Sort(data_sets)
-
     return 0;
 }
-
