2f986ce8cb5956217fffa150e11d66387fd8aec1
Will <will.green98@hotmail.com>
2018-05-28 22:54:35 +0100

    Algorith updates

--- Main-Algorithms/main.cpp
+++ Main-Algorithms/main.cpp
@@ -12,11 +12,6 @@
 
 #include "nlohmann/json.hpp"
 
-#include "Bubble_Sort.h"
-#include "Selection_Sort.h"
-#include "Insertion_Sort.h"
-#include "Quick_Sort.h"
-
 using json = nlohmann::json;
 std::ifstream data("/Data/data_sets.json");
 
@@ -27,11 +22,6 @@ int main(int argc, char * argv[]) {
     std::vector<int> data_arr_one = json["data_sets"]["data_arr_one"];
     std::vector<int> data_arr_two = json["data_sets"]["data_arr_two"];
     std::vector<int> data_arr_three = json["data_sets"]["data_arr_three"];
-    
-    bubble_sort(&data_arr_one);
-    selection_sort(&data_arr_one);
-    insertion_sort(&data_arr_one);
-    quick_sort(&data_arr_one);
-    
+
     return 0;
 }
