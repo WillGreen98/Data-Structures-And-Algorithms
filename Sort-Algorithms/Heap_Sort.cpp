be5557af8ba9e77f3e982e67b6be701d570b9455
Shwillybum <will.green98@hotmail.com>
2018-06-13 19:27:49 +0100

    Heap Sort

---
+++ Sort-Algorithms/Heap_Sort.cpp
@@ -0,0 +1,34 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#include <iostream>
+
+#include "Heap_Sort.h"
+
+Heap_Sort::Heap_Sort() {
+    std::cout << "I am a Heap Sort Algorithm" << std::endl;
+}
+
+void heap(int d[], int x, int index) {
+    int largest = index;
+    int left_wall = 2 * index + 1;
+    int right_wall = 2 * index + 2;
+
+    if(left_wall < x && d[left_wall] > d[largest]) { largest = left_wall; }
+    if(right_wall < x && d[right_wall] > d[largest]) {largest = right_wall; }
+
+    if(largest != index) {
+        swap(d[index], d[largest]);
+        heap(d, x, largest);
+    }
+}
+
+void heap_sort(int d[], int x) {
+    for(int i = n / 2 - 1; i >= 0; i--) { heap(d, x, i); }
+
+    for(int i = n-1; i >= 0; i--) {
+        swap(d[0], d[i]);
+        heap(d, i, 0);
+    }
+}
---
+++ Sort-Algorithms/Heap_Sort.h
@@ -0,0 +1,16 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_HEAP_SORT_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_HEAP_SORT_H
+
+class Heap_Sort {
+    public:
+        Heap_Sort();
+};
+
+void heap(int d[], int x, int index);
+void heap_sort(int d[], int x);
+
+#endif //DATA_STRUCTURES_AND_ALGORITHMS_HEAP_SORT_H
