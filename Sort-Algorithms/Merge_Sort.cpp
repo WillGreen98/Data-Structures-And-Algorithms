bfd49254eb4a1ae78d19f1ad0eb6795769c452bd
Shwillybum <will.green98@hotmail.com>
2018-06-13 19:12:28 +0100

    Merge Sort

---
+++ Sort-Algorithms/Merge_Sort.cpp
@@ -0,0 +1,58 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#include <iostream>
+
+#include "Merge_Sort.h"
+
+Merge_Sort::Merge_Sort() {
+    std::cout << "I am a Merge Sort Algorithm" << std::endl;
+}
+
+void merge(int d[], int left_wall, int middle_bound, int right_wall) {
+    int a = 0;
+    int b = 0;
+    int temp = left_wall;
+
+    int n1 = middle_bound - left_wall + 1;
+    int n2 = right_wall - middle_bound;
+
+    int L[n1], R[n2];
+
+    for(a = 0; a < n1; a++) { L[a] = d[left_wall + a]; }
+    for(b = 0; b < n2; b++) { R[b] = d[middle_bound + 1 + b]; }
+
+    while(a < n1 && b < n2) {
+        if (L[a] <= R[b]) {
+            d[temp] = L[a];
+            a++;
+        } else {
+            d[k] = R[b];
+            b++;
+        }
+        temp++;
+    }
+
+    while(a < n1) {
+        d[k] = L[a];
+        a++;
+        temp++;
+    }
+
+    while(b < n2) {
+        d[temp] = R[b];
+        b++;
+        temp++;
+    }
+}
+
+void mergeSort(int d[], int left_wall, int right_wall) {
+    if(left_wall < right_wall) {
+        int middle = left_wall + (right_wall-left_wall) / 2;
+
+        mergeSort(d, left_wall, middle);
+        mergeSort(d, middle + 1, right_wall);
+        merge(d, left_wall, middle, right_wall);
+    }
+}
---
+++ Sort-Algorithms/Merge_Sort.h
@@ -0,0 +1,13 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_MERGE_SORT_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_MERGE_SORT_H
+
+
+class Merge_Sort {
+    public: Merge_Sort();
+};
+
+#endif //DATA_STRUCTURES_AND_ALGORITHMS_MERGE_SORT_H
