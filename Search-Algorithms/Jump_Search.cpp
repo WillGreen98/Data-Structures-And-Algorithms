4f24c720d1f37524a2c0c0299ee303797ab59864
Shwillybum <will.green98@hotmail.com>
2018-06-13 05:57:17 +0100

    Added Jump Search

---
+++ Search-Algorithms/Jump_Search.cpp
@@ -0,0 +1,36 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#include <iostream>
+
+#include "Jump_Search.h"
+
+Jump_Search::Jump_Search() {
+    std::cout << "I am a Jump Search Algorithm" << std::endl;
+};
+
+int jumpSearch(int d[], int x, int index) {
+    int step = sqrt(index);
+    int prev = 0;
+
+    while(d[min(step, index)-1] < x) {
+        prev = step;
+        step += sqrt(index);
+        if (prev >= index)
+            return -1;
+    }
+
+    while (d[prev] < x) {
+        prev++;
+
+        if (prev == min(step, index))
+            return -1;
+    }
+
+    if(d[prev] == x) {
+        return prev;
+    }
+
+    return -1;
+}
---
+++ Search-Algorithms/Jump_Search.h
@@ -0,0 +1,17 @@
+//
+// Created by Will  on 13/06/2018.
+//
+
+#ifndef DATA_STRUCTURES_AND_ALGORITHMS_JUMP_SEARCH_H
+#define DATA_STRUCTURES_AND_ALGORITHMS_JUMP_SEARCH_H
+
+
+class Jump_Search {
+    public:
+        Jump_Search();
+};
+
+int linear_search(int d[], int x ,int index);
+
+
+#endif //DATA_STRUCTURES_AND_ALGORITHMS_JUMP_SEARCH_H
