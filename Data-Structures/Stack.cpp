06a1522419268404fbb3920fd82fa88b03addd52
Shwillybum <will.green98@hotmail.com>
2018-06-20 06:15:09 +0100

    Restructure of class

--- Data-Structures/Stack.cpp
+++ Data-Structures/Stack.cpp
@@ -27,11 +27,10 @@ Node newNode(T *d_value) {
 
 template<typename T>
 class Stack_List {
-    int d_value;
-    int top;
-
     public:
-        int d_max[500];
+        static const int d_max[500];
+        T *d_value;
+        int top;
 
         Stack_List() {
             top = -1;
@@ -41,13 +40,13 @@ class Stack_List {
         if(top < 0 ) {
             return 0;
         } else {
-            T d_value = d_max[top--];
-            return d_value;
+            this->d_value = (T *)d_max[top--];
+            return reinterpret_cast<int>(d_value);
         }
     };
 
     bool push(T *d_value) {
-        if(top > 500) { // d_max is weird
+        if(top > *d_max) { // d_max is weird
             return false;
         } else {
             *d_value = d_max[top++];
