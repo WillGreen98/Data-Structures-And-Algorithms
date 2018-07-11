6b8b000f0617f87eb6ac736eec32634e519d8725
WillGreen98 <will.green98@hotmail.com>
2018-07-11 18:48:29 +0100

    Update

--- Data-Structures/Heap/Binary_Heap.cpp
+++ Data-Structures/Heap/Binary_Heap.cpp
@@ -2,4 +2,31 @@
 // Created by Will  on 08/07/2018.
 //
 
+#include <iostream>
+#include <utility>
+
 #include "Binary_Heap.h"
+
+Binary_Heap::Binary_Heap(int capacity) {
+    std::cout << "I am an example of a Binary Heap" << std::endl;
+
+    head_size = 0;
+    capacity = capacity;
+    heap_array = new int[capacity];
+}
+
+void Binary_Heap::insert_key(int key_value) {
+    if(head_size == capacity) {
+        return;
+    }
+
+    head_size++;
+    int i = head_size - 1;
+    heap_array[i] = key_value;
+
+    while(i != 0 && heap_array[parent(i)] > heap_array[i]) {
+        std::swap(&heap_array[i], &heap_array[parent(i)]);
+        i = parent(i);
+    }
+}
+
--- Data-Structures/Heap/Binary_Heap.h
+++ Data-Structures/Heap/Binary_Heap.h
@@ -5,10 +5,24 @@
 #ifndef DATA_STRUCTURES_AND_ALGORITHMS_BINARY_HEAP_H
 #define DATA_STRUCTURES_AND_ALGORITHMS_BINARY_HEAP_H
 
-
 class Binary_Heap {
+    int *heap_array;
+    int head_size;
+    int capacity;
 
+    public:
+        Binary_Heap(int capacity);
+        void insert_key(int key_value);
+        int parent(int i) { return (i-1)/2; }
+        int left(int i) { return (2*i + 1); }
+        int right(int i) { return (2*i + 2); }
+
+        int extractMin();
+
+        void decreaseKey(int i, int new_val);
+        int getMin() { return harr[0]; }
+        void deleteKey(int i);
+        void insertKey(int k);
 };
-
 
 #endif //DATA_STRUCTURES_AND_ALGORITHMS_BINARY_HEAP_H
